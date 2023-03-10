9561250c9e7ccba50171cde24f189057e30bdfc2
SergioAlias <sergio08071998@gmail.com>
2023-03-10 13:08:11 +0100

    Add comention and coex results filter

---
+++ annotations/comention/get_comentions.R
@@ -0,0 +1,69 @@
+#!/usr/bin/env Rscript
+
+# Sergio Alías, 20230310
+# Last modified 20230310
+
+# Script for getting commentions that explain 
+# We need:
+#   - Coex analysis results -> wil_results-[HPA/TabulaSapiens]-[tissue].tsv
+#   - Comention files -> ALL_CL-HPO_add_s.tsv /// ALL_ACT-HPO_add_s.tsv
+
+
+### Packages
+
+library(data.table)
+
+
+### Input files setup
+
+tissue <- "blood"
+dataset <- "HPA"
+
+results <- fread(file.path("../../coex-analysis/",
+                           dataset,
+                           tissue,
+                           paste0("wil_results_",
+                                  dataset,
+                                  "_",
+                                  tissue,
+                                  ".tsv")
+                           )
+                 )
+
+results <- results[wicoxon.sign != "ns"]
+
+
+comentions <- fread("ALL_ACT-HPO_add_s.tsv")
+
+colnames(comentions) <- c("ACT.id",
+                          "ACT.name",
+                          "HPO.id",
+                          "HPO.name",
+                          "str.sim",
+                          "mentions.ACT",
+                          "mentions.HPO",
+                          "comentions",
+                          "ratio",
+                          "p.val")
+
+### Filtering comention file by HPO terms
+
+HPO.comentions <- comentions[HPO.id %in% unique(gsub("HP", "HP:", results$hpo))]
+
+rm(comentions)
+
+### Check if the comention is between the HPO and the desired cell type
+
+
+sign.results <- data.table()
+
+for (i in 1:nrow(results)){
+  for (j in 1:nrow(HPO.comentions)){
+    if (gsub("HP", "HP:", results[i, hpo]) == HPO.comentions[j, HPO.id]){
+      if (results[i, annotation] == HPO.comentions[j, ACT.name]){
+        row.to.add <- cbind(results[i], HPO.comentions[j, list(str.sim, comentions, ratio, p.val)])
+        sign.results <- rbind(sign.results, row.to.add)
+      }
+    }
+  }
+}
