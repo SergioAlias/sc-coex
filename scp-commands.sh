50c2b6631d6c0959b3a341b8158873330999b45d
SergioAlias <sergio08071998@gmail.com>
2023-03-21 10:18:43 +0100

    Deleted file

--- scp-commands.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-scp -pr annotations/ salias@etna:/home/salias/TFM/
-scp -pr coex-analysis/ salias@etna:/home/salias/TFM/
-scp COTAN/results/COTAN-cluster-script.R salias@etna:/home/salias/TFM/COTAN/results
-scp COTAN/results/coex-to-tsv.R salias@etna:/home/salias/TFM/COTAN/results
