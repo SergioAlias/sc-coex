57eb528d6e9fb9b8d9e96306b2c06693bf4f8d4a
SergioAlias <sergio08071998@gmail.com>
2023-03-23 14:39:56 +0100

    Added code to DEsingle.R for handling HPA

--- test-TiPA/DEsingle.R
+++ test-TiPA/DEsingle.R
@@ -1,16 +1,25 @@
 #!/usr/bin/env Rscript
 
 # Sergio Alías, 20230321
-# Last modified 20230321
+# Last modified 20230323
 
 # Script for runnig DE analysis on single-cell count matrices using R package DEsingle
 
 ## Imports
 
 library(DEsingle)
+library(data.table)
 
 ## Main script
 
 data(TestData)
 results <- DEsingle(counts = counts, group = group)
 results.classified <- DEtype(results = results, threshold = 0.05)
+results.sig <- results.classified[results.classified$pvalue.adj.FDR < 0.05, ]
+
+
+data <- fread("TFM/rna_single_cell_read_count.tsv")
+ann <- data[, c(1:3)]
+counts <- data[, -c(1:3)]
+rm(data)
+c(1:nrow(ann))[ann[,1] == "Adipose" & ann[,3] == 0]
