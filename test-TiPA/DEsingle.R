ecb10c538691a9d3ea8e5d15963a8018aee9809a
SergioAlias <sergio08071998@gmail.com>
2023-03-23 16:25:49 +0100

    Updated DEsingle

--- test-TiPA/DEsingle.R
+++ test-TiPA/DEsingle.R
@@ -10,16 +10,35 @@
 library(DEsingle)
 library(data.table)
 
-## Main script
+## Test data
 
 data(TestData)
 results <- DEsingle(counts = counts, group = group)
 results.classified <- DEtype(results = results, threshold = 0.05)
 results.sig <- results.classified[results.classified$pvalue.adj.FDR < 0.05, ]
 
+## Main script
 
-data <- fread("TFM/rna_single_cell_read_count.tsv")
+data <- fread("TFM/rna_single_cell_read_count.tsv") # Read HPA dataset
 ann <- data[, c(1:3)]
 counts <- data[, -c(1:3)]
 rm(data)
-c(1:nrow(ann))[ann[,1] == "Adipose" & ann[,3] == 0]
+
+# c(1:nrow(ann))[ann[,1] == "Liver" & ann[,3] == 0] # For checking
+
+### Filter the tissue (e.g. Liver)
+
+counts <- counts[c(1:nrow(ann))[ann[,1] == "Liver"],]
+ann <- ann[c(1:nrow(ann))[ann[,1] == "Liver"],]
+
+
+### Preparing the input for DEsingle
+
+# counts is a count matrix or an sce object
+# The rows of the matrix are genes and columns are cells
+
+counts <- as.data.frame(counts)
+counts <- t(counts)
+
+# group is a vector of factor which specifies the two groups in the matrix to be compared, corresponding to the columns in counts
+
