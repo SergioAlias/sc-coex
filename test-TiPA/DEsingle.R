feebb54999434231d068dfa86a4cbc200b0d58a5
SergioAlias <sergio08071998@gmail.com>
2023-03-24 12:48:48 +0100

    DEsigle.R works with HPA dataset

--- test-TiPA/DEsingle.R
+++ test-TiPA/DEsingle.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 
 # Sergio Alías, 20230321
-# Last modified 20230323
+# Last modified 20230324
 
 # Script for runnig DE analysis on single-cell count matrices using R package DEsingle
 
@@ -12,14 +12,16 @@ library(data.table)
 
 ## Test data
 
-data(TestData)
-results <- DEsingle(counts = counts, group = group)
-results.classified <- DEtype(results = results, threshold = 0.05)
-results.sig <- results.classified[results.classified$pvalue.adj.FDR < 0.05, ]
+# data(TestData)
+# results <- DEsingle(counts = counts, group = group)
+# results.classified <- DEtype(results = results, threshold = 0.05)
+# results.sig <- results.classified[results.classified$pvalue.adj.FDR < 0.05, ]
 
 ## Main script
 
-data <- fread("TFM/rna_single_cell_read_count.tsv") # Read HPA dataset
+tissue <- "Liver"
+
+data <- fread("../rna_single_cell_read_count.tsv") # Read HPA dataset
 ann <- data[, c(1:3)]
 counts <- data[, -c(1:3)]
 rm(data)
@@ -28,8 +30,8 @@ rm(data)
 
 ### Filter the tissue (e.g. Liver)
 
-counts <- counts[c(1:nrow(ann))[ann[,1] == "Liver"],]
-ann <- ann[c(1:nrow(ann))[ann[,1] == "Liver"],]
+counts <- counts[c(1:nrow(ann))[ann[,1] == tissue],]
+ann <- ann[c(1:nrow(ann))[ann[,1] == tissue],]
 
 
 ### Preparing the input for DEsingle
@@ -37,8 +39,16 @@ ann <- ann[c(1:nrow(ann))[ann[,1] == "Liver"],]
 # counts is a count matrix or an sce object
 # The rows of the matrix are genes and columns are cells
 
-counts <- as.data.frame(counts)
 counts <- t(counts)
 
 # group is a vector of factor which specifies the two groups in the matrix to be compared, corresponding to the columns in counts
 
+group <- numeric(length = nrow(ann))
+group[which(ann[,3] == 0)] <- 1
+group[which(ann[,3] != 0)] <- 2
+group <- as.factor(group)
+
+
+results <- DEsingle(counts = counts, group = group)
+results.classified <- DEtype(results = results, threshold = 0.05)
+results.sig <- results.classified[results.classified$pvalue.adj.FDR < 0.05, ]
