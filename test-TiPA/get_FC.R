707dfe301598c8fbd0d142399030e327ca8ba7d4
SergioAlias <sergio08071998@gmail.com>
2023-04-18 20:15:12 +0200

    Will be working tomorrow

--- test-TiPA/get_FC.R
+++ test-TiPA/get_FC.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 
 # Sergio Alías, 20230417
-# Last modified 20230417
+# Last modified 20230418
 
 # Script for obtaining the FC of genes on the pooled RNAseq HPA dataset
 
@@ -11,7 +11,9 @@ library(data.table)
 
 ### Main script ###
 
-RNAseq <- fread("../../rna_single_cell_type_tissue.tsv") # Read data
+RNAseq <- fread("../rna_single_cell_type_tissue.tsv") # Read data
+
+colnames(RNAseq)[c(2, 5)] <- c("Gene_name", "Cell_type") # Change some colnames with spaces
 
 RNAseq[, sum_pTPM := sum(pTPM), by = .(Gene, Tissue)] # Create a new column with the sum of pTPM for each gene and tissue
 
@@ -19,4 +21,24 @@ RNAseq[, n_clusters := uniqueN(Cluster), by = .(Gene, Tissue)] # Create a new co
 
 RNAseq[, log2FC := log2(pTPM / ((sum_pTPM - pTPM) / (n_clusters - 1)))] # Calculate log2 fold-change for each row
 
-RNAseq[, c("sum_pTPM", "n_clusters") := NULL] # Remove intermediate columns
+# RNAseq[, c("sum_pTPM", "n_clusters") := NULL] # Remove intermediate columns
+
+
+
+
+
+# Compute the total pTPM and number of clusters for each Gene, Tissue, and Cell_type combination
+RNAseq[, `:=` (sum_pTPM_per_ct = sum(pTPM), n_clusters_per_ct = .N), by = .(Gene, Tissue, Cell_type)]
+
+# Create a new column with the sum of pTPM for each Gene and Tissue combination, excluding the current row's Cell_type value
+RNAseq[, sum_pTPM_single_tissue := sum_pTPM - sum_pTPM_per_ct, by = .(Gene, Tissue)]
+
+# Create a new column with the count of clusters for each Gene and Tissue combination, excluding the current row's Cell_type value
+RNAseq[, n_clusters_single_tissue := n_clusters - 1, by = .(Gene, Tissue)]
+
+# Create a new column with the log2 fold-change for each row, relative to other clusters with different Cell_type values
+RNAseq[, log2FC_single_tissue := log2(pTPM / sum_pTPM_single_tissue) / log2(n_clusters_single_tissue)]
+
+# Remove the temporary columns
+RNAseq[, c("sum_pTPM", "n_clusters", "sum_pTPM_single_tissue", "n_clusters_single_tissue") := NULL]
+
